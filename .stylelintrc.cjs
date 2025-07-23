4fe6fc5aa4c2712126c1142dcf282d40a90b28df
alex-o-drag <alexandr.dragan.jur@gmail.com>
2025-07-23 22:35:43 +0300

    Add prettier and stylelint

---
+++ .stylelintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  customSyntax: 'postcss-scss',
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['use'],
+      },
+    ],
+    'color-hex-length': 'long',
+  },
+};
